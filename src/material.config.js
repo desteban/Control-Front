dbcfd17b1fb4c8c9ae393533eb0c720fbf5c6505
desteban <esteban28c@hotmail.com>
2019-10-15 12:49:02 -0500

    login

--- src/material.config.js
+++ src/material.config.js
@@ -16,18 +16,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.Dropdown.init(elems);
 });
 
+function ocultar(){  
 
-function ocultar(){
-  console.log('Hide')
-  var elementos = document.getElementById('mobile-demo')
-  //ocultar menu
-  elementos.style.transform = "translateX(-150%)"
-
-  //esconder div
-  var elementos = document.getElementsByClassName('sidenav-overlay')
-  if(elementos.length > 0){
-    for (let i = 0; i < elementos.length; i++)
-      elementos[i].style.cssText = "display: none; opacity: 0;";
-  }
-
+  console.log('Hide menu')
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    instances.close()
 }
