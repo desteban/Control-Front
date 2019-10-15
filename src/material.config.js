f4c479b7db6bb7a5d4b220f5ba18f795e45ac00b
desteban <esteban28c@hotmail.com>
2019-10-15 11:59:29 -0500

    ocultar menu

--- src/material.config.js
+++ src/material.config.js
@@ -15,3 +15,19 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.dropdown-trigger');
   var instances = M.Dropdown.init(elems);
 });
+
+
+function ocultar(){
+  console.log('Hide')
+  var elementos = document.getElementById('mobile-demo')
+  //ocultar menu
+  elementos.style.transform = "translateX(-150%)"
+
+  //esconder div
+  var elementos = document.getElementsByClassName('sidenav-overlay')
+  if(elementos.length > 0){
+    for (let i = 0; i < elementos.length; i++)
+      elementos[i].style.cssText = "display: none; opacity: 0;";
+  }
+
+}
