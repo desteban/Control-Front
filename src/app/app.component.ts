6ffbcf541847cb82d475a4f45fca97bcb1568c14
desteban <esteban28c@hotmail.com>
2019-10-18 23:51:15 -0500

    refrescar menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from "./services/UserService";
+
+/**
+ * DoCheck permite realizar acciones cuando re registre un cambio en la aplcacion
+ * por ejemplo si borramos o escribimos datos (localstorage)
+ */
 
 @Component({
   selector: 'app-root',
@@ -7,16 +12,31 @@ import { UserService } from "./services/UserService";
   styleUrls: ['./app.component.scss'],
   providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
 
+  
   public title = 'Control-Front';
   public token;
   public identity
-
+  
   constructor(
     public _userService: UserService
-  ){
-    this.token = this._userService.getToken()
-    this.identity = this._userService.getIdentity()
+    ){
+      this.getIdentity()
+    }
+    
+    ngOnInit() {
+      this.getIdentity()
+      console.log('app cargada')
+    }
+
+    ngDoCheck() {
+      this.getIdentity()
+    }
+
+    private getIdentity(){
+      this.token = this._userService.getToken()
+      this.identity = this._userService.getIdentity()
+    }
+
   }
-}
